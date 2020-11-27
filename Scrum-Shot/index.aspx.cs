7e8d7d915c06f7cb3f0b64588b00ec1236b814ca
Nikolaus Böhm <Nikolaus.BOEHM@htlvb.at>
2020-11-27 11:18:16 +0100

    Erstellen der BenutzerKlassen

--- Scrum-Shot/index.aspx.cs
+++ Scrum-Shot/index.aspx.cs
@@ -13,5 +13,64 @@ namespace Scrum_Shot
         {
 
         }
+
+    }
+    abstract class User
+    {
+        public User(int _userID, int _persID, string _firstname, string _lastname, string _email)
+        {
+            this.userID = _userID;
+            this.persID = _persID;
+            this.firstname = _firstname;
+            this.lastname = _lastname;
+            this.email = _email;
+        }
+
+
+
+        protected int userID { get;}
+        protected int persID { get;}
+        protected string firstname { get;}
+        protected string lastname { get;}
+        protected string email { get;}
+        protected string password { get;}
+        protected int priority { get; set; }
+
+        List<Project> projects;
+
+    }
+    class Admin : User
+    {
+        public Admin(int _userID, int _persID, string _firstname, string _lastname, string _email) : base(_userID,_persID,_firstname,_lastname,_email)
+        {
+            priority = 0;
+        }
+    }
+    class ScrumMaster : User
+    {
+        public ScrumMaster(int _userID, int _persID, string _firstname, string _lastname, string _email) : base(_userID, _persID, _firstname, _lastname, _email)
+        {
+            priority = 1;
+        }
+    }
+    class ProductOwner : User
+    {
+        public ProductOwner(int _userID, int _persID, string _firstname, string _lastname, string _email) : base(_userID, _persID, _firstname, _lastname, _email)
+        {
+            priority = 2;
+        }
+    }
+    class TeamMember : User
+    {
+        public TeamMember(int _userID, int _persID, string _firstname, string _lastname, string _email) : base(_userID, _persID, _firstname, _lastname, _email)
+        {
+            priority = 3;
+        }
+    }
+    public class Project
+    {
+        int projectID;
+        string projectName;
+
     }
 }
