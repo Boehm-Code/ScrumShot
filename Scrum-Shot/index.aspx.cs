aa6ecb083f8b9e6416c6f64b9517223fe60d5101
Nikolaus Böhm <Nikolaus.BOEHM@htlvb.at>
2020-12-04 09:14:46 +0100

    Hinzufügen einer Backlogklasse

--- Scrum-Shot/index.aspx.cs
+++ Scrum-Shot/index.aspx.cs
@@ -91,4 +91,28 @@ namespace Scrum_Shot
         public string ProjectName { get; set; }
         public List<User> Participants { get; }
     }
+
+    public class ProductBacklog
+    {
+        List<ProductBacklogPoint> backlogPoints;
+
+    }
+    public class ProductBacklogPoint
+    {
+        string description;
+        int storyPoints;
+        bool inProgress;
+        bool done;
+        User editedBy;
+
+
+
+
+
+        public string Description{ get; set; }
+        public int StoryPoints { get; set; }
+        public bool InProgress { get; set; }
+        public bool Done { get; set; }
+        public User EditedBy{ get; set; }
+    }
 }
